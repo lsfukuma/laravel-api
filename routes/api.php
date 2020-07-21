4892220bc1bf56e00c87a733aa89fbbb882938bd
Leticia <lsf@protonmail.com>
2020-07-21 17:05:50 +0200

    API access with token

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,19 @@ Route::middleware('apitoken')->namespace('Api')->group(function() {
     Route::post('/movies', 'MovieController@index');
     Route::post('/movies/{movie}', 'MovieController@show');
 });
-
+// Route::group([
+//     'prefix' => 'auth'
+// ], function () {
+//     Route::post('login', 'AuthController@login');
+//     Route::post('signup', 'AuthController@signup');
+//
+//     Route::group([
+//       'middleware' => 'auth:api'
+//     ], function() {
+//         Route::get('logout', 'AuthController@logout');
+//         Route::get('user', 'AuthController@user');
+//     });
+// });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
